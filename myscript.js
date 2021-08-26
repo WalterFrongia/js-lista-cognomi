8fef588b5bfaf3d5ecf6cee2c7e6dc89380fdd41
WalterFrongia <walter.frongia@libero.it>
2021-08-26 17:42:44 +0200

    Aggiunto in js ordine alfabetico della lista dell'array e indicata la posizione del cognome utente che viene inserito

--- myscript.js
+++ myscript.js
@@ -3,27 +3,22 @@
 let cognomeUtente = prompt("inserisci il tuo cognome");
 console.log(cognomeUtente);
 
-
 // inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
 
 let cognomiInLista = [ "Bianchi" , "Neri" , "Rossi" , "Verdi" , "Gialli"];
+console.log(cognomiInLista);
+
+cognomiInLista.push(cognomeUtente); //Prima qui avevo inserito un altro cognome -> "Speciale" spiegare.
 
 console.log(cognomiInLista);
 
-cognomiInLista.push(cognomeUtente); //Prima qui avevo inserito un altro cognome -> "Speciale" spiegare. 
-
-// if (cognomeUtente == cognomiInLista){
-//     console.log("puoi accedere");
-// }else{
-//     console.log("non sei in lista");
-// }
-
-
-
 // stampa la lista ordinata alfabeticamente
 
-
-
+cognomiInLista.sort()
+console.log(cognomiInLista);
 
 
 // scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
+
+const posizioneCognomeUtente = cognomiInLista.indexOf(cognomeUtente) +1;
+console.log(posizioneCognomeUtente);
